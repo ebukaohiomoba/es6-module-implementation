71bdbc84acca9838d86341602de600c7a1d2ba5b
Ebuka Ohiomoba <chukwuebukaohiomoba@gmail.com>
2022-03-14 13:06:49 -0700

    Create employee class

---
+++ script.js
@@ -0,0 +1,8 @@
+class Employee {
+    constructor(name, id, permissions, storenumber){
+        this.name = name;
+        this.id = id;
+        this.permissions = permissions;
+        this.storenumber = storenumber;
+    }
+}
