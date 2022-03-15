0ef2dfb920c626b8eaf91f8153c33fe87af9be2e
Ebuka Ohiomoba <chukwuebukaohiomoba@gmail.com>
2022-03-15 06:55:47 -0700

    Added comments

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 class Employee {
-    constructor(name, id, permissions, storenumber){
-        this.name = name;
-        this.id = id;
-        this.permissions = permissions;
+    constructor(employeename, employeeid, employeepermissions, storenumber){
+        this.employeename = employeename;
+        this.employeeid = employeeid;
+        this.permissions = employeepermissions;
         this.storenumber = storenumber;
+    }
+}
+
+class Manager extends Employee {
+    constructor(employeename, employeeid, employeepermissions, storenumber, employeelist,){
+        super(employeename,employeeid,employeepermissions,storenumber);
+        this.employeesmanaged = [];
+            addnewemployee = function (employeename){
+                this.employeesmanaged.push(employeename);
+                return employeesmanaged;
+            } 
+
     }
 }
